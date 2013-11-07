7624b7940adc0a38f5c592a0700e1a4e17555f5b
2011531010 <2011531010@ma246.is.env.kitakyu-u.ac.jp>
2013-11-07 15:26:34 +0900

    Initial Commit

---
+++ aTests/aTests.h
@@ -0,0 +1,13 @@
+//
+//  aTests.h
+//  aTests
+//
+//  Created by 大野　有以 on 13/11/07.
+//  Copyright (c) 2013年 univercityofkitakyu. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface aTests : SenTestCase
+
+@end
---
+++ aTests/aTests.m
@@ -0,0 +1,32 @@
+//
+//  aTests.m
+//  aTests
+//
+//  Created by 大野　有以 on 13/11/07.
+//  Copyright (c) 2013年 univercityofkitakyu. All rights reserved.
+//
+
+#import "aTests.h"
+
+@implementation aTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in aTests");
+}
+
+@end
